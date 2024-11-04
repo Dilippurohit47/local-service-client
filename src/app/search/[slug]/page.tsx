95a75ec92d884c574ea49b4cddd5d8fd4292dd36
Dilippurohit47 <dilippurohit204@gmail.com>
2024-11-04 19:43:57 +0530

    search resuk api

--- src/app/search/[slug]/page.tsx
+++ src/app/search/[slug]/page.tsx
@@ -1,11 +1,36 @@
 "use client";
+import ServicamanCard, {
+  serviceType,
+} from "@/components/my-components/ServicamanCard";
+import { fetcher } from "@/lib/utils";
 import { useParams } from "next/navigation";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import useSWR from "swr";
 
 const page = () => {
   const { slug } = useParams();
-  console.log(slug);
-  return <div>dddddddddd</div>;
+  const [services, setServices] = useState<serviceType[]>([]);
+  const { data, error } = useSWR(
+    `${process.env.NEXT_PUBLIC_SERVER}/api/v1/services/get/${slug}`,
+    fetcher
+  );
+  useEffect(() => {
+    if (data) {
+      setServices(data.data);
+    }
+  }, [data]);
+  return (
+    <div className="flex  flex-col gap-4 ml-5 w-full h-full mt-20">
+      <h1 className="font-bold  text-3xl">{slug}</h1>
+      <div className="flex gap-4">
+        {services.length > 0
+          ? services.map((item, index) => (
+              <ServicamanCard key={index} item={item} />
+            ))
+          : "no services"}
+      </div>
+    </div>
+  );
 };
 
 export default page;
--- src/components/my-components/ServicamanCard.tsx
+++ src/components/my-components/ServicamanCard.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { Star } from "lucide-react";
 
-interface serviceType {
+export interface serviceType {
   name: string;
   profileUrl: string;
   services: string[];
 }
 
 const ServicamanCard = ({ item }: { item: serviceType }) => {
+  console.log("item",item)
   return (
     <div className="   w-64 h-70 bg-white   rounded-2xl border-2 cursor-pointer px-2 py-3 flex flex-col gap-3 ">
       <div className="flex justify-center items-center  ">
         <img
-          src={item.profileUrl}
+          src={item?.profileUrl}
           alt="img"
           className="object-center rounded-2xl  h-40 w-full  "
         />
@@ -20,14 +21,14 @@ const ServicamanCard = ({ item }: { item: serviceType }) => {
 
       <div className=" px-3  leading-tight ">
         <div className="flex  justify-between items-center">
-          <h4 className="font-medium text-[1.1rem]">{item.name}</h4>
+          <h4 className="font-medium text-[1.1rem]">{item?.name}</h4>
           <h5 className="  flex gap-1 items-center">
             4.5 <Star className="text-yellow-400 fill-yellow-400" size={15} />
           </h5>
         </div>
         <div className="flex w-3/4 gap-2   ">
           <p className="truncate overflow-hidden ">
-            {item.services.map((i, index) => (
+            {item?.services?.map((i, index) => (
               <span key={index}>{i},</span>
             ))}
           </p>
